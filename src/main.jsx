686c055ec76c9933c9d0f33b7f3f24b61d74f47a
Poly4 <olajuwonmubarak355@gmail.com>
2023-07-31 22:50:25 +0100

    add google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
 
+
+ReactGA.initialize("G-KN9ZYCC0X2");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
